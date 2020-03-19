91f13798dd95194fe15edccd9283a7b5624bd692
min920822 <kings789@naver.com>
2020-03-19 15:18:58 +0900

    프래그먼트를 => MainActivity위에 올려줌

--- app/src/main/java/com/example/a20200319_01_fragment/fragments/RoomListFragment.java
+++ app/src/main/java/com/example/a20200319_01_fragment/fragments/RoomListFragment.java
@@ -11,9 +11,19 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.a20200319_01_fragment.R;
+import com.example.a20200319_01_fragment.adapters.RoomAdapter;
 import com.example.a20200319_01_fragment.databinding.FragmentRoomListBinding;
+import com.example.a20200319_01_fragment.fragments.datas.Room;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomListFragment extends Fragment {
+
+    List<Room> roomList = new ArrayList<>();
+
+    RoomAdapter adapter = null;
+
     FragmentRoomListBinding binding = null;
 
     @Nullable
@@ -26,6 +36,13 @@ public class RoomListFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        adapter = new RoomAdapter(getActivity(),
+                R.layout.room_list_item,roomList);
+
+        binding.roomListView.setAdapter(adapter);
+        roomList.add(new Room(5000,"서울시 은평구"));
+        adapter.notifyDataSetChanged();
 
     }
 }
